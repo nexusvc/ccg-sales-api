1cd7f408f2483bac8fa844e65f6c32277246f1bf
John Paul Medina <jp@leadtrust.io>
2022-05-12 20:30:10 -0400

    Update UcaAddOn

--- src/CcgSalesApi/Product/Types/UcaAddOn.php
+++ src/CcgSalesApi/Product/Types/UcaAddOn.php
@@ -2,10 +2,27 @@
 
 namespace Nexusvc\CcgSalesApi\Product\Types;
 
-use Nexusvc\CcgSalesApi\Product\GenericProduct;
+use Nexusvc\CcgSalesApi\Product\LimitedMedical;
 
 class UcaAddOn extends LimitedMedical {
 
-    
+    protected $uri;
+
+    protected static $params = [
+        'npn',
+        'state',
+        'effectiveDate',
+        'dateOfBirth',
+        'coverageType',
+        'minPrice',
+        'maxPrice',
+        'age',
+        'isUCAAddOn'
+    ];
+
+    protected $required = [
+        'npn',
+        'state'
+    ];
 
 }
